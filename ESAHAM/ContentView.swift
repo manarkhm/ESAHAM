038d9e17cc31b9b51d404c45c97284da0dc9f555
manarkhm <manar.kh.almalki@gmail.com>
2024-01-23 14:09:39 +0300

    Initial Commit

---
+++ ESAHAM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ESAHAM
+//
+//  Created by manar khaled on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ESAHAM/ESAHAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESAHAMApp.swift
+//  ESAHAM
+//
+//  Created by manar khaled on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ESAHAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
